d4f99fae687da3f249b73adbdc114b4a73715cd7
John Melas <john@jmelas.gr>
2023-11-04 14:15:37 +0200

    format

--- Source/Engine/Tuning.h
+++ Source/Engine/Tuning.h
@@ -13,16 +13,16 @@
 class Tuning
 {
 private:
-    MTSClient* mts_client{nullptr};
-    enum Mode {
+    MTSClient * mts_client {nullptr};
+    enum Mode
+    {
         MTS_ESP,
         TWELVE_TET
-    } mode{TWELVE_TET};
-    
+    } mode {TWELVE_TET};
+
 public:
     Tuning()
     {
- 
     }
     ~Tuning()
     {
@@ -32,7 +32,7 @@ public:
             mts_client = nullptr;
         }
     }
-    
+
     void updateMTSESPStatus()
     {
         if (mts_client == nullptr)
@@ -42,39 +42,38 @@ public:
 
         mode = hasMTSMaster() ? MTS_ESP : TWELVE_TET;
     }
-    
+
     double midiNoteFromMTS(int midiIndex)
     {
         return midiIndex + MTS_RetuningInSemitones(mts_client, midiIndex, -1);
     }
-   
+
     double tunedMidiNote(int midiIndex)
     {
-        switch(mode)
+        switch (mode)
         {
-            case TWELVE_TET:
-                return midiIndex;
-                break;
-            case MTS_ESP:
-                return midiNoteFromMTS(midiIndex);
-                break;
+        case TWELVE_TET:
+            return midiIndex;
+            break;
+        case MTS_ESP:
+            return midiNoteFromMTS(midiIndex);
+            break;
         }
         return midiIndex;
     }
-    
+
     /*
         These methods can be later be used for implementing other steps in the MTS-ESP guide:
         https://github.com/ODDSound/MTS-ESP/blob/main/Client/libMTSClient.h
      */
-    
+
     bool hasMTSMaster()
     {
         return MTS_HasMaster(mts_client);
     }
-    
-    const char *getMTSScale()
+
+    const char * getMTSScale()
     {
         return MTS_GetScaleName(mts_client);
     }
-    
 };
